6bc7345e7bd4db8bdef0f566038b6f94610fb6b0
Owner <Owner@DESKTOP-6UVR642>
2022-11-03 21:55:13 -0400

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module christine.fxmlexample {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens christine.fxmlexample to javafx.fxml;
+    exports christine.fxmlexample;
+}
+*/
